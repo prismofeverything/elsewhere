2b398b7e19851dec893234680922d3b75bc24caa
Ryan Spangler <ryan.spangler@gmail.com>
2025-04-10 18:03:28 -0700

    started on fundamental operations

--- elsewhere/effect.py
+++ elsewhere/effect.py
@@ -1,1 +1,19 @@
+from effectful.ops.syntax import defop, defdata
+
+
+@defop
+def nest(outer: bigraph, inner: bigraph) -> bigraph:
+    raise NotImplementedError(f'nest not implemented')
+
+@defop
+def merge(left: bigraph, right: bigraph) -> bigraph:
+    raise NotImplementedError(f'merge not implemented')
+
+@defop
+def parallel(left: bigraph, right: bigraph) -> bigraph:
+    raise NotImplementedError(f'parallel not implemented')
+
+@defop
+def link(node: bigraph, edge: edge) -> bigraph:
+    raise NotImplementedError(f'link not implemented')
 
